3d2f710b4a16e2aa7a98508eb45821c54c2e6f2c
Aviral Mehrotra <73425338+AviralMehrotra@users.noreply.github.com>
2026-07-18 11:10:09 +0530

    feat: initialize FastAPI service and commit versioned model and vectorizer artifacts

--- src/api/main.py
+++ src/api/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.inference.predictor import (
     predict_category
 )
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
